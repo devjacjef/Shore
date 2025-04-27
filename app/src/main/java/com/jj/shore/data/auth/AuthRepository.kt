9f0ced19e216b8357dc6c46cca85e0b53fcc64f1
Jack Jefferson <immxral.j4ck@gmail.com>
2025-04-27 22:48:37 +0100

    Added Auth Data

---
+++ app/src/main/java/com/jj/shore/data/auth/AuthRepository.kt
@@ -0,0 +1,32 @@
+package com.jj.shore.data.auth
+
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class AuthRepository @Inject constructor(
+    private val authRemoteDataSource: AuthRemoteDataSource
+) {
+    val currentUser: FirebaseUser? = authRemoteDataSource.currentUser
+    val currentUserId: Flow<String?> = authRemoteDataSource.currentUserIdFlow
+
+    suspend fun createGuestAccount() {
+        authRemoteDataSource.createGuestAccount()
+    }
+
+    suspend fun signIn(email: String, password: String) {
+        authRemoteDataSource.signIn(email, password)
+    }
+
+    suspend fun signUp(email: String, password: String) {
+        authRemoteDataSource.linkAccount(email, password)
+    }
+
+    fun signOut() {
+        authRemoteDataSource.signOut()
+    }
+
+    suspend fun deleteAccount() {
+        authRemoteDataSource.deleteAccount()
+    }
+}
