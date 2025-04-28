23c58f33ade45e4a97733ce33dd4bc5c76a86045
Jack Jefferson <immxral.j4ck@gmail.com>
2025-04-28 21:15:53 +0100

    Updated UI of Login Screen

--- app/src/main/java/com/jj/shore/ui/login/LoginScreen.kt
+++ app/src/main/java/com/jj/shore/ui/login/LoginScreen.kt
@@ -1,13 +1,25 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.jj.shore.ui.login
 
 import android.widget.Space
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -15,10 +27,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.credentials.PasswordCredential
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -32,36 +49,79 @@ import kotlinx.coroutines.launch
 fun LoginScreen(
     viewModel: LoginViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
-
     val isLoading = remember { mutableStateOf(false) }
-
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
-    Column {
+
+    Box(
+        Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LoginForm(
+            email = email,
+            password = password,
+            onEmailChange = { email = it },
+            onPasswordChange = { password = it },
+            onSignInClick = {
+                isLoading.value = true
+                viewModel.signIn(email, password)
+            }
+        )
+    }
+
+}
+
+@Composable
+fun LoginForm(
+    email: String,
+    password: String,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onSignInClick: () -> Unit
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text("Shore", fontSize = 64.sp)
+
+        Spacer(Modifier.size(16.dp))
+
         TextField(
             value = email,
-            onValueChange = { email = it },
-            label = { Text("john.doe@gmail.com")}
+            onValueChange = onEmailChange,
+            label = { Text("Email") },
+            shape = RoundedCornerShape(12.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent
+            )
         )
 
         Spacer(Modifier.size(16.dp))
 
         TextField(
             value = password,
-            onValueChange = { password = it },
-            label = { Text("Your Password")},
+            onValueChange = onPasswordChange,
+            label = { Text("Password") },
+            shape = RoundedCornerShape(12.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent
+            ),
             visualTransformation = PasswordVisualTransformation()
         )
 
         Spacer(Modifier.size(32.dp))
 
-        Button(onClick = {
-            isLoading.value = true
-            viewModel.signIn(email, password)
-        }) {
+        Button(onClick = onSignInClick,
+            Modifier.height(48.dp).width(200.dp)) {
             Text("Sign In")
         }
     }
-
 }
